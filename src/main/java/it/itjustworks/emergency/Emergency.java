ec141ce26fc662ada8a57e472ca18504e6e6bb01
Riccardo Crippa <therickys93@gmail.com>
2017-04-05 15:18:25 +0200

    primo commit

---
+++ src/main/java/it/itjustworks/emergency/Emergency.java
@@ -0,0 +1,7 @@
+package it.itjustworks.emergency;
+
+public class Emergency {
+
+	
+	
+}
---
+++ src/test/java/it/itjustworks/emergency/EmergencyTest.java
@@ -0,0 +1,14 @@
+package it.itjustworks.emergency;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmergencyTest {
+
+	@Test
+	public void testOne() {
+		assertEquals("", "");
+	}
+	
+}
