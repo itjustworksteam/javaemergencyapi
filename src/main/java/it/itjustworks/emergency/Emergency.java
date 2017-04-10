da9067d50dcf619570b61635682011498e6b4221
Riccardo Crippa <therickys93@gmail.com>
2017-04-10 17:34:27 +0200

    add possibility to change the backend url

--- src/main/java/it/itjustworks/emergency/Emergency.java
+++ src/main/java/it/itjustworks/emergency/Emergency.java
@@ -9,15 +9,24 @@ import okhttp3.Response;
 public class Emergency {
 
 	private OkHttpClient client;
-	private static String ENDPOINT = "https://emergency-server.herokuapp.com";
+	private String endpoint;
 	
 	public Emergency() {
+		this("https://emergency-server.herokuapp.com");
+	}
+	
+	private Emergency(String backendUrl){
 		this.client = new OkHttpClient();
+		this.endpoint = backendUrl;
+	}
+	
+	public Emergency withBackEndUrl(String url){
+		return new Emergency(url);
 	}
 	
 	public String sendRequest(Requestable req) throws IOException {
-		Request request = new Request.Builder().url(ENDPOINT + req.url()).build();
-		Response response = client.newCall(request).execute();
+		Request request = new Request.Builder().url(this.endpoint + req.url()).build();
+		Response response = this.client.newCall(request).execute();
 		return response.body().string();
 	}
 
